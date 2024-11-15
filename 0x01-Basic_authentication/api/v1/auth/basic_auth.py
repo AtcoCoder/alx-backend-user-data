14b0d00649ae63a234ca6036761d76bf9d716db0
Omar Jammeh <orms45@gmail.com>
2024-11-15 23:23:30 +0000

    Add user_object_from_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -56,3 +58,30 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':')
         return email, password
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Return:
+            - None if user_email is None or not a string
+            - None if user_pwd is None or not a string
+            - None if your database (file) is doesn't contain
+            any User instance with email to user_email
+            - None if user_pwd is not the password of the User
+            instance found
+            - Otherwise, User instance
+        """
+        if not user_email or type(user_email) != str:
+            return None
+        if not user_pwd or type(user_pwd) != str:
+            return None
+        users = User.search({'email': user_email})
+        if not users or len(users) == 0:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
