ed8573a53b652146dab0bf15e211b89198e3b308
Omar Jammeh <orms45@gmail.com>
2024-11-18 09:17:41 +0000

    Add user module

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" User
+"""
+sqlalchemy import Column, Integer, String
+
+
+class User:
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
