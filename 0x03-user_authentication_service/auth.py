6ee1c23c19c8487bf978cac3c420f4d6a38982fc
Omar Jammeh <orms45@gmail.com>
2024-11-18 13:48:18 +0000

    Add resgister_user method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,10 @@
 """Auth
 """
 import bcrypt
+from db import DB
+from typing import TypeVar
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +13,31 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(byts, salt)
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Instance initializer
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """
+        Args:
+            - email <string>
+            - password <string>
+        Return:
+            - User object
+        """
+        try:
+            user_exist = self._db.find_user_by(email=email)
+            print("user_exist")
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        hashed_pwd = _hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=hashed_pwd)
+        return user
