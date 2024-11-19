ec49715aee9140beb65a8e56ff724285509ecaf1
Omar Jammeh <orms45@gmail.com>
2024-11-19 00:44:52 +0000

    Implement reset token route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,17 @@ def profile() -> str:
     return abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def reset_password() -> str:
+    """Reset password route
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        return abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
