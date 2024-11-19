897bcb5a16427fd10fbb215d55184de7d4cb9c33
Omar Jammeh <orms45@gmail.com>
2024-11-19 21:13:28 +0000

    Add route /reset_password to update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,19 @@ def reset_password() -> str:
         return abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password() -> str:
+    """update password route
+    """
+    email = request.form.get('email')
+    new_password = request.form.get('new_passowrd')
+    reset_token = request.form.get('reset_token')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        return abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
