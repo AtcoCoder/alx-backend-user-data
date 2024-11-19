70047a0feae539387318c978a9437a4fc183f3ea
Omar Jammeh <orms45@gmail.com>
2024-11-19 00:38:38 +0000

    Add get_reset_password_token method to the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,7 +81,7 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
-    
+
     def get_user_from_session_id(self, session_id: str) -> TypeVar('User'):
         """Finds user from session id
         Args:
@@ -97,7 +97,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: str) -> None:
         """Sets session id of user to None
         Args:
@@ -111,3 +111,18 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Resets the token of user
+        Args:
+            - email <string>
+        Return:
+            - new token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            return ValueError
