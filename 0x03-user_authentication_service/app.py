151813737fe26d293bbb406b0880943d83b3ffa8
Omar Jammeh <orms45@gmail.com>
2024-11-22 11:54:59 +0000

    Correct password key typo

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,8 +87,9 @@ def update_password() -> str:
     """update password route
     """
     email = request.form.get('email')
-    new_password = request.form.get('new_passowrd')
+    new_password = request.form.get('new_password')
     reset_token = request.form.get('reset_token')
+    print(new_password)
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"})
